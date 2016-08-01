17d510c7d9d20e21afdfb73bdeac8b85f8977b8f
David Lohmeyer <vilepickle@gmail.com>
2016-08-01 14:57:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo module to override the Drupal 8 contact module mail handler
+
+This is an example of how to change the default behavior of the Drupal 8 contact mail handler by sending emails to different people based on a field's value.
---
+++ example_contact/src/ExampleMailHandler.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\example_contact;
+
+use Drupal\contact\MessageInterface;
+use Drupal\contact\MailHandler as ContactMailHandler;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Provides a class for handling assembly and dispatch of contact mail messages.
+ */
+class ExampleMailHandler extends ContactMailHandler {
+
+  /**
+   * The department selected, if applicable.
+   *
+   * @var string
+   */
+  protected $department;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function sendMailMessages(MessageInterface $message, AccountInterface $sender) {
+    $contact_form = $message->getContactForm();
+
+    // If it's not the contact form just do the normal mail handler.
+    if($contact_form->id() != 'contact') {
+      parent::sendMailMessages($message, $sender);
+      return;
+    }
+
+    $this->department = NULL;
+
+    // Find Departmental contact
+    $this->department = $message->get('field_contact_department')->getValue()[0]['value'];
+
+    if($this->department !== NULL) {
+      $this->setDepartmentRecipients($contact_form);
+    }
+
+    parent::sendMailMessages($message, $sender);
+
+  }
+
+  /**
+   * Set recipients for a contact form based on departmental
+   * field selection for field_contact_department.
+   *
+   * @param Drupal\contact\Entity\ContactForm
+   *   The contact form to modify.
+   */
+  public function setDepartmentRecipients(&$contact_form) {
+
+    switch($this->department) {
+      case 'web':
+        $recipients = ['example@example.com'];
+        break;
+      case 'general':
+        $recipients = ['example@example.com', 'example2@example.com'];
+        break;
+      case 'blog':
+        $recipients = ['blog@example.com'];
+        break;
+      default:
+        $recipients = ['me@me.com'];
+    }
+
+    $contact_form->setRecipients($recipients);
+
+  }
+
+}
